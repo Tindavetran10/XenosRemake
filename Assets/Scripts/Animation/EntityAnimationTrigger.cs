e63ed2f9f89d3496b01ed5642d554451478eb5f0
Tindavetran10 <102245594+Tindavetran10@users.noreply.github.com>
2025-07-31 02:35:27 +0700

    Fix Enemy cannot do damage to Player

--- Assets/Scripts/Animation/EntityAnimationTrigger.cs
+++ Assets/Scripts/Animation/EntityAnimationTrigger.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 public class EntityAnimationTrigger : MonoBehaviour
 {
     private Entity _entity;
-    private EntityCombatModify _entityCombat;
+    private EntityCombat _entityCombat;
     private bool _shouldSkipRemainingAnimation;
     
     public virtual void Awake()
     {
         _entity = GetComponentInParent<Entity>();
         //_entityCombat = GetComponentInParent<EntityCombat>();
-        _entityCombat = GetComponentInParent<EntityCombatModify>();
+        _entityCombat = GetComponentInParent<EntityCombat>();
     }
 
     public virtual void CurrentStateTrigger() => _entity.CurrentStateAnimationTrigger();
     public virtual void CurrentVelocityStateTrigger() => _entity.CallVelocityAnimationTrigger();
     public virtual void StopVelocityStateTrigger() => _entity.CallStopVelocityAnimationTrigger();
-    //public virtual void AttackTrigger() => _entityCombat.PerformAttack();
+    public virtual void AttackTrigger() => _entityCombat.PerformAttack();
     
     // Updated to support attack index
-    public virtual void AttackTrigger() => AttackTrigger(0);
-    public virtual void AttackTrigger(int attackIndex) => _entityCombat.PerformAttack(attackIndex);
+    /*public virtual void AttackTrigger() => AttackTrigger(0);
+    public virtual void AttackTrigger(int attackIndex) => _entityCombat.PerformAttack(attackIndex);*/
 }
